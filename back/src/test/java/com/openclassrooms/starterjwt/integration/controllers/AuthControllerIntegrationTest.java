e5791aa70b3ad0b90664b74a8ba43d75bd9b86a4
axelker <axelker@outlook.fr>
2025-02-22 20:47:33 +0100

    fix: use sql instead repository for insert data

--- back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/AuthControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/AuthControllerIntegrationTest.java
@@ -1,9 +1,7 @@
 package com.openclassrooms.starterjwt.integration.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,15 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.payload.request.SignupRequest;
-import com.openclassrooms.starterjwt.repository.UserRepository;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.instancio.junit.InstancioExtension;
@@ -31,35 +26,19 @@ import org.instancio.junit.InstancioExtension;
 @ExtendWith(InstancioExtension.class)
 public class AuthControllerIntegrationTest {
     private final String basePath = "/api/auth/";
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserRepository userRepository;
+    private final String userEmail ="test@test.com";
+    private final String userPassword = "test!1234";
 
     @Autowired
     private MockMvc mockMvc;
-    
-    private User testUser;
 
-    @BeforeEach
-    void setUp() {
-        testUser = new User();
-        testUser.setEmail("test@test.com");
-        testUser.setPassword(passwordEncoder.encode("123456"));
-        testUser.setFirstName("John");
-        testUser.setLastName("Doe");
-        testUser.setAdmin(true);
-        userRepository.save(testUser);
-    }
 
     @Test
     @DisplayName("POST /login - With valid credentials should return JWT and user info.")
     void login_WithValidCredentials_ShouldReturnJwtAndUserInfo() throws Exception {
         LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setEmail("test@test.com");
-        loginRequest.setPassword("123456");
+        loginRequest.setEmail(userEmail);
+        loginRequest.setPassword(userPassword);
 
         mockMvc.perform(post(basePath + "login")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -67,17 +46,18 @@ public class AuthControllerIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.token").isString())
                 .andExpect(jsonPath("$.type").value("Bearer"))
-                .andExpect(jsonPath("$.username").value(testUser.getEmail()))
-                .andExpect(jsonPath("$.firstName").value(testUser.getFirstName()))
-                .andExpect(jsonPath("$.lastName").value(testUser.getLastName()))
-                .andExpect(jsonPath("$.admin").value(true));
+                .andExpect(jsonPath("$.username").value(userEmail))
+                .andExpect(jsonPath("$.firstName").value("Admin"))
+                .andExpect(jsonPath("$.lastName").value("Admin"))
+                .andExpect(jsonPath("$.admin").value(true))
+                .andExpect(jsonPath("$.password").doesNotExist());
     }
 
     @Test
     @DisplayName("POST /login - With invalid credentials should return 401 Unauthorized.")
     void login_WithInvalidCredentials_ShouldReturnUnauthorized() throws Exception {
         LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setEmail("test@test.com");
+        loginRequest.setEmail(userEmail);
         loginRequest.setPassword("wrongpassword");
 
         mockMvc.perform(post(basePath + "/login")
@@ -90,7 +70,7 @@ public class AuthControllerIntegrationTest {
     @DisplayName("POST /login - With missing login fields should return 400 Bad request.")
     void login_WithMissingLoginFields_ShouldReturnBadRequest() throws Exception {
         LoginRequest requestMissingEmail = new LoginRequest();
-        requestMissingEmail.setPassword("123456");
+        requestMissingEmail.setPassword(userPassword);
 
         mockMvc.perform(post(basePath + "/login")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -98,7 +78,7 @@ public class AuthControllerIntegrationTest {
                 .andExpect(status().isBadRequest());
 
         LoginRequest requestMissingPassword = new LoginRequest();
-        requestMissingPassword.setEmail("test@test.com");
+        requestMissingPassword.setEmail(userEmail);
 
         mockMvc.perform(post(basePath + "/login")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -121,7 +101,6 @@ public class AuthControllerIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("User registered successfully!"));
 
-        assertThat(userRepository.findByEmail("register@test.com")).isNotEmpty();
     }
 
     
@@ -131,7 +110,7 @@ public class AuthControllerIntegrationTest {
         SignupRequest registerRequest = new SignupRequest();
         registerRequest.setFirstName("registerfirstname");
         registerRequest.setLastName("registerlastname");
-        registerRequest.setEmail("test@test.com");
+        registerRequest.setEmail(userEmail);
         registerRequest.setPassword("123456");
 
         mockMvc.perform(post(basePath + "register")
--- back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/UserControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/UserControllerIntegrationTest.java
@@ -1,9 +1,7 @@
 package com.openclassrooms.starterjwt.integration.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,40 +13,28 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.instancio.Instancio;
-import org.instancio.junit.InstancioExtension;
-
-import com.openclassrooms.starterjwt.models.User;
-import com.openclassrooms.starterjwt.repository.UserRepository;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @Transactional
 @AutoConfigureMockMvc
-@ExtendWith(InstancioExtension.class)
 public class UserControllerIntegrationTest {
+    private final String basePath = "/api/user/";
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private UserRepository userRepository;
 
 
     @Test
     @DisplayName("GET /api/user/{id} - With existing user id should return user.")
     @WithMockUser(username = "test@test.com")
     void findById_WithExistingUser_ShouldReturnUser() throws Exception {
-        User userSaved = Instancio.create(User.class);
-        userSaved.setEmail("test@test.com");
-        userSaved = userRepository.save(userSaved);
-        mockMvc.perform(get("/api/user/" + userSaved.getId()))
+        mockMvc.perform(get(basePath + 1))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.email").value(userSaved.getEmail()))
-            .andExpect(jsonPath("$.firstName").value(userSaved.getFirstName()))
-            .andExpect(jsonPath("$.lastName").value(userSaved.getLastName()))
-            .andExpect(jsonPath("$.admin").value(userSaved.isAdmin()))
+            .andExpect(jsonPath("$.email").value("test@test.com"))
+            .andExpect(jsonPath("$.firstName").value("Admin"))
+            .andExpect(jsonPath("$.lastName").value("Admin"))
+            .andExpect(jsonPath("$.admin").value(true))
             .andExpect(jsonPath("$.password").doesNotExist());
     }
 
@@ -56,7 +42,7 @@ public class UserControllerIntegrationTest {
     @DisplayName("GET /api/user/{id} - Should return 404 Not Found when user does not exist.")
     @WithMockUser(username = "test@test.com")
     void findById_WithNonExistingUserId_ShouldReturnNotFound() throws Exception {
-        mockMvc.perform(get("/api/user/" + 9999))
+        mockMvc.perform(get(basePath + 9999))
             .andExpect(status().isNotFound());
     }
 
@@ -64,7 +50,7 @@ public class UserControllerIntegrationTest {
     @DisplayName("GET /api/user/{id} - Should return 400 Bad Request when ID is not a number.")
     @WithMockUser(username = "test@test.com")
     void findById_WithNotIdNumber_ShouldReturnBadRequest() throws Exception {
-        mockMvc.perform(get("/api/user/" + "bonjournombre"))
+        mockMvc.perform(get(basePath + "bonjournombre"))
             .andExpect(status().isBadRequest());
     }
 
@@ -72,12 +58,8 @@ public class UserControllerIntegrationTest {
     @DisplayName("DELETE /api/user/{id} - Should delete user when authenticated user matches target user")
     @WithMockUser(username = "test@test.com")
     void deleteById_WhenAuthenticatedUserMatchesTargetUser_ShouldDeleteUser() throws Exception {
-        User userSaved = Instancio.create(User.class);
-        userSaved.setEmail("test@test.com");
-        userSaved = userRepository.save(userSaved);
-        mockMvc.perform(delete("/api/user/" + userSaved.getId()))
+        mockMvc.perform(delete(basePath + 1))
             .andExpect(status().isOk());
-        assertThat(userRepository.findById(userSaved.getId())).isEmpty();
 
     }
 
@@ -85,10 +67,7 @@ public class UserControllerIntegrationTest {
     @DisplayName("DELETE /api/user/{id} - Should return 401 Unauthorized when authenticated user does not match target user")
     @WithMockUser(username = "baduser@test.com")
     void deleteById_WhenAuthenticatedUserDoesNotMatchTargetUser_ShouldReturnUnauthorized() throws Exception {    
-        User userSaved = Instancio.create(User.class);
-        userSaved.setEmail("test@test.com");
-        userSaved = userRepository.save(userSaved);
-        mockMvc.perform(delete("/api/user/" + userSaved.getId()))
+        mockMvc.perform(delete(basePath + 1))
             .andExpect(status().isUnauthorized());
 
     }
@@ -97,7 +76,7 @@ public class UserControllerIntegrationTest {
     @DisplayName("DELETE /api/user/{id} - Should return 404 Not Found when user does not exist.")
     @WithMockUser(username = "test@test.com")
     void deleteById_WithNonExistingUserId_ShouldReturnNotFound() throws Exception {
-        mockMvc.perform(delete("/api/user/" + 9999))
+        mockMvc.perform(delete(basePath + 9999))
             .andExpect(status().isNotFound());
     }
 
@@ -105,29 +84,7 @@ public class UserControllerIntegrationTest {
     @DisplayName("DELETE /api/user/{id} - Should return 400 Bad Request when ID is not a number.")
     @WithMockUser(username = "test@test.com")
     void deleteById_WithNotIdNumber_ShouldReturnBadRequest() throws Exception {
-        mockMvc.perform(delete("/api/user/" + "bonjournombre"))
+        mockMvc.perform(delete(basePath + "bonjournombre"))
             .andExpect(status().isBadRequest());
-    }
-    // @DeleteMapping("{id}")
-    // public ResponseEntity<?> save(@PathVariable("id") String id) {
-    //     try {
-    //         User user = this.userService.findById(Long.valueOf(id));
-
-    //         if (user == null) {
-    //             return ResponseEntity.notFound().build();
-    //         }
-
-    //         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-    //         if(!Objects.equals(userDetails.getUsername(), user.getEmail())) {
-    //             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    //         }
-
-    //         this.userService.delete(Long.parseLong(id));
-    //         return ResponseEntity.ok().build();
-    //     } catch (NumberFormatException e) {
-    //         return ResponseEntity.badRequest().build();
-    //     }
-    // }
-    
+    }    
 }
