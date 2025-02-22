1b37c80c847b3f0856572d522e15923de2f25344
axelker <axelker@outlook.fr>
2025-02-22 21:34:39 +0100

    feat: add participate data

--- back/src/test/resources/data.sql
+++ back/src/test/resources/data.sql
@@ -7,3 +7,10 @@ INSERT INTO USERS (first_name, last_name, admin, email, password)
 VALUES ('Admin', 'Admin', true, 'test@test.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq'),
         ('User', 'User', false, 'user@test.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq');
 
+INSERT INTO SESSIONS (id, name, description, date, teacher_id) VALUES
+(1, 'Yoga1', 'test', '2025-03-01 10:00:00', 1),
+(2, 'Yoga2', 'test', '2025-03-02 14:00:00', 2);
+
+INSERT INTO PARTICIPATE (user_id, session_id) VALUES
+(1, 1),
+(1, 2);
