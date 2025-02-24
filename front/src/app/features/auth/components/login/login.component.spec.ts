aa606b3b4d8ef035957e08508bca609514da7a7c
axelker <axelker@outlook.fr>
2025-02-24 22:11:22 +0100

    feat: add test for login and register component

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,15 +11,23 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { By } from '@angular/platform-browser';
+import { of, throwError } from 'rxjs';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
+  let router: Router;
+  let sessionService: SessionService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [{ provide: Router, useValue: { navigate: jest.fn() } }],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -33,10 +41,71 @@ describe('LoginComponent', () => {
       .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    sessionService = TestBed.inject(SessionService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the login title', () => {
+    const titleElement = fixture.debugElement.query(By.css('mat-card-title')).nativeElement;
+    expect(titleElement.textContent.trim()).toBe('Login');
+  });
+
+  it('should disable submit button if form is invalid', () => {
+    component.form.patchValue({ email: 'tesest.com', password: '' });
+    component.form.updateValueAndValidity();
+    fixture.detectChanges();
+    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
+    expect(submitButton.disabled).toBeTruthy();
+
+    component.form.patchValue({ email: 'test@test.com', password: 'pass' });
+    component.form.updateValueAndValidity();
+    fixture.detectChanges();
+
+    expect(submitButton.disabled).toBeFalsy();
+  });
+
+  it('should set session info and navigate to sessions on submit login success', () => {
+    const mockSession: SessionInformation = {
+        id: 1,
+        username: 'testUser',
+        token: 'bearer',
+        type: 'test',
+        firstName: 'firstname',
+        lastName: 'lastname',
+        admin: true,
+      };
+    jest.spyOn(authService, 'login').mockReturnValue(of(mockSession));
+    jest.spyOn(sessionService, 'logIn');
+    jest.spyOn(router, 'navigate');
+
+
+    component.submit();
+
+    fixture.detectChanges();
+
+    expect(sessionService.logIn).toHaveBeenCalledWith(mockSession);
+    expect(router.navigate).toHaveBeenCalledWith(['/sessions']);
+
+  });
+
+  it('should display error message on submit login failure', () => {
+    jest.spyOn(authService, 'login').mockReturnValue(throwError(() => new Error('Login failed')));
+    jest.spyOn(sessionService, 'logIn');
+
+    component.submit();
+
+    fixture.detectChanges();
+
+    const errorElement = fixture.debugElement.query(By.css('.error')).nativeElement;
+    expect(component.onError).toBeTruthy();
+    expect(errorElement).toBeTruthy();
+    expect(errorElement.textContent.trim()).toBe('An error occurred');
+    expect(sessionService.logIn).not.toHaveBeenCalled();
   });
 });
--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,32 +9,165 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { By } from '@angular/platform-browser';
+import { of, throwError } from 'rxjs';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
+  let router: Router;
+
+  const validFormValue = {
+    email: 'test@example.com',
+    firstName: 'test',
+    lastName: 'test',
+    password: 'securepassword',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
+      providers: [{ provide: Router, useValue: { navigate: jest.fn() } }],
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
-      ]
-    })
-      .compileComponents();
+        MatInputModule,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the register title', () => {
+    const titleElement = fixture.debugElement.query(
+      By.css('mat-card-title')
+    ).nativeElement;
+    expect(titleElement.textContent.trim()).toBe('Register');
+  });
+
+  describe('Form Validation - Submit Button State', () => {
+    describe('Invalid', () => {
+      it('should disable submit button if email is invalid', () => {
+        const invalidEmailFormValue = {
+          ...validFormValue,
+          email: 'tesest.com',
+        };
+
+        component.form.patchValue(invalidEmailFormValue);
+        component.form.updateValueAndValidity();
+        fixture.detectChanges();
+  
+        const submitButton = fixture.debugElement.query(
+          By.css('button[type="submit"]')
+        ).nativeElement;
+        expect(submitButton.disabled).toBeTruthy();
+      });
+
+      it('should disable submit button if firstname is invalid', () => {
+        const invalidFirstnameFormValue = {
+          ...validFormValue,
+          firstName: '',
+        };
+  
+        component.form.patchValue(invalidFirstnameFormValue);
+        component.form.updateValueAndValidity();
+        fixture.detectChanges();
+
+        const submitButton = fixture.debugElement.query(
+          By.css('button[type="submit"]')
+        ).nativeElement;
+        expect(submitButton.disabled).toBeTruthy();
+      });
+
+      it('should disable submit button if lastname is invalid', () => {
+        const invalidLastnameFormValue = {
+          ...validFormValue,
+          lastName: '',
+        };
+
+        component.form.patchValue(invalidLastnameFormValue);
+        component.form.updateValueAndValidity();
+        fixture.detectChanges();
+
+        const submitButton = fixture.debugElement.query(
+          By.css('button[type="submit"]')
+        ).nativeElement;
+        expect(submitButton.disabled).toBeTruthy();
+      });
+
+      it('should disable submit button if password is invalid', () => {
+        const invalidPasswordFormValue = {
+          ...validFormValue,
+          password: '123',
+        };
+
+        component.form.patchValue(invalidPasswordFormValue);
+        component.form.updateValueAndValidity();
+        fixture.detectChanges();
+
+        const submitButton = fixture.debugElement.query(
+          By.css('button[type="submit"]')
+        ).nativeElement;
+        expect(submitButton.disabled).toBeTruthy();
+      });
+    });
+
+    describe('Valid', () => {
+      it('should enable submit button', () => {
+        component.form.patchValue(validFormValue);
+        component.form.updateValueAndValidity();
+        fixture.detectChanges();
+
+        const submitButton = fixture.debugElement.query(
+          By.css('button[type="submit"]')
+        ).nativeElement;
+        expect(submitButton.disabled).toBeFalsy();
+      });
+    });
+  });
+
+  describe('Submit form', () => {
+    it('should navigate to login on register success', () => {
+      jest.spyOn(authService, 'register').mockReturnValue(of({} as any));
+      jest.spyOn(router, 'navigate');
+      component.submit();
+
+      fixture.detectChanges();
+
+      expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    });
+
+    it('should display error message on register failure', () => {
+      jest
+        .spyOn(authService, 'register')
+        .mockReturnValue(throwError(() => new Error('Register failed')));
+      jest.spyOn(router, 'navigate');
+      component.submit();
+
+      fixture.detectChanges();
+
+      const errorElement = fixture.debugElement.query(
+        By.css('.error')
+      ).nativeElement;
+      expect(component.onError).toBeTruthy();
+      expect(errorElement).toBeTruthy();
+      expect(errorElement.textContent.trim()).toBe('An error occurred');
+      expect(router.navigate).not.toHaveBeenCalled();
+    });
   });
 });
