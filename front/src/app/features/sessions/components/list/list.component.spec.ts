03ae5782176e3850798c7ef2bbc81eef94bf55c8
axelker <axelker@outlook.fr>
2025-02-25 10:37:45 +0100

    feat: add test for session list component

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -1,4 +1,3 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,31 +5,134 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { ListComponent } from './list.component';
+import { SessionApiService } from '../../services/session-api.service';
+import { By } from '@angular/platform-browser';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { Session } from '../../interfaces/session.interface';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
-
-  const mockSessionService = {
-    sessionInformation: {
-      admin: true
-    }
-  }
+  let sessionApiService: SessionApiService;
+  let sessionService: SessionService;
+  const mockSessionInformation: SessionInformation = {
+    id: 1,
+    username: 'testUser',
+    token: 'bearer',
+    type: 'test',
+    firstName: 'firstname',
+    lastName: 'lastname',
+    admin: false,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListComponent],
-      imports: [HttpClientModule, MatCardModule, MatIconModule],
-      providers: [{ provide: SessionService, useValue: mockSessionService }]
-    })
-      .compileComponents();
+      imports: [HttpClientTestingModule, RouterTestingModule, MatCardModule, MatIconModule],
+      providers: [
+        {
+          provide: SessionService,
+          useValue: { sessionInformation: mockSessionInformation },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
+    sessionApiService = TestBed.inject(SessionApiService);
+    sessionService = TestBed.inject(SessionService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the Session title', () => {
+    const titleElement = fixture.debugElement.query(By.css('mat-card-title')).nativeElement;
+    expect(titleElement.textContent.trim()).toBe('Rentals available');
+  });
+
+  describe('Create button',() => {
+    it('should not display on non admin user', () => {
+      sessionService.sessionInformation = { admin: false } as any;
+      fixture.detectChanges();
+  
+      const createButton = getCreateButton(fixture);
+      expect(createButton).toBeNull();
+    });
+
+      it('Should display on admin user', () => {
+        sessionService.sessionInformation= { admin: true } as any;
+        fixture.detectChanges();
+  
+        const createButton = getCreateButton(fixture);
+        expect(createButton).not.toBeNull();
+      });
+  
+  });
+
+  
+
+  describe('Session list',() => {
+    it('should display sessions from the observable', () => {
+      const mockSessions: Session[] = [
+        { id: 1,
+          name: 'session1',
+          description: 'description',
+          date: new Date('2023-05-12'),
+          teacher_id: 1,
+          users: [],},
+        {  id: 2,
+          name: 'session2',
+          description: 'description',
+          date: new Date('2023-05-12'),
+          teacher_id: 1,
+          users: [],}
+      ];
+  
+      jest.spyOn(sessionApiService, 'all').mockReturnValue(of(mockSessions));
+      component.ngOnInit();
+      fixture.detectChanges();
+      const sessionElements = fixture.debugElement.queryAll(By.css('.item'));
+
+      expect(sessionElements.length).toBe(2);
+      expect(sessionElements[0].nativeElement.textContent).toContain('session1');
+      expect(sessionElements[1].nativeElement.textContent).toContain('session2');
+    });
+
+    it('should display the Edit button on session item only for admins', () => {
+      sessionService.sessionInformation = { admin: true } as any;
+    
+      const mockSessions: Session[] = [
+        { id: 1, name: 'session1', description: 'description', date: new Date(), teacher_id: 1, users: [] }
+      ];
+    
+      jest.spyOn(sessionApiService, 'all').mockReturnValue(of(mockSessions));
+      
+      component.ngOnInit();
+      fixture.detectChanges();
+    
+      const editButton = fixture.debugElement.query(By.css('[data-test-id="edit-session-1"]'));
+      
+      expect(editButton).toBeTruthy();
+      expect(editButton.nativeElement.textContent).toContain('Edit');
+    });
+    
+  });
 });
+
+function getCreateButton(fixture: ComponentFixture<ListComponent>) {
+  return fixture.debugElement.query(
+    By.css('[data-test-id="create-session-button"]')
+  );
+}
+
+function getEditButton(fixture: ComponentFixture<ListComponent>) {
+  return fixture.debugElement.query(
+    By.css('[data-test-id="create-session-button"]')
+  );
+}
--- front/src/app/features/sessions/components/list/list.component.ts
+++ front/src/app/features/sessions/components/list/list.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
 import { SessionInformation } from '../../../../interfaces/sessionInformation.interface';
 import { SessionService } from '../../../../services/session.service';
 import { Session } from '../../interfaces/session.interface';
@@ -10,15 +10,17 @@ import { SessionApiService } from '../../services/session-api.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
-  public sessions$: Observable<Session[]> = this.sessionApiService.all();
+  public sessions$: Observable<Session[]> = EMPTY;
 
   constructor(
     private sessionService: SessionService,
     private sessionApiService: SessionApiService
   ) { }
-
+  ngOnInit() {
+    this.sessions$ = this.sessionApiService.all();
+  }
   get user(): SessionInformation | undefined {
     return this.sessionService.sessionInformation;
   }
