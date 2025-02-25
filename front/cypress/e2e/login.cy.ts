4e9cddd3ae3ff207e0ea2ca3aac16c71be524fbb
axelker <axelker@outlook.fr>
2025-02-25 17:12:33 +0100

    feat: add login and register e2e tests

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,5 +1,7 @@
-describe('Login spec', () => {
-  it('Login successfull', () => {
+/// <reference types="Cypress" />
+
+describe('Login Page', () => {
+  it('Should redirects to the sessions page after a successful login', () => {
     cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
@@ -24,4 +26,34 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
+
+  it('Should displays an error message when login fails', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: { message: 'Invalid credentials' }
+    }).as('loginError');
+    
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.url().should('include', '/login')
+    cy.get('.error').should('be.visible').and('contain', "An error occurred");
+  });
+
+  it('Should disable submit form button when invalid form', () => {
+    cy.visit('/login')
+
+    
+    cy.get('input[formControlName=email]').type("invalid.com").blur();
+    cy.get('input[formControlName=password]').focus().blur();
+    
+
+    cy.get('input[formControlName=email]').should('have.class', 'ng-invalid');
+    cy.get('input[formControlName=password]').should('have.class', 'ng-invalid');
+    cy.url().should('include', '/login')
+    cy.get('button[type=submit]').should('be.disabled');
+  })
+  
 });
